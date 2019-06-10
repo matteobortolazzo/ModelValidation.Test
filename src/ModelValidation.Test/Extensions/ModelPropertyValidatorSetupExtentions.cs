e72846e8c1533a539ceeb5df53eb68be9fb422c0
Matteo Bortolazzo <dev@matteobortolazzo.com>
2019-06-10 22:07:11 +0100

    Changed library to .NET Standard.

--- src/ModelValidation.Test/Extensions/ModelPropertyValidatorSetupExtentions.cs
+++ src/ModelValidation.Test/Extensions/ModelPropertyValidatorSetupExtentions.cs
@@ -9,23 +9,43 @@ namespace ModelValidation.Test.Extensions
         public static IModelPropertyValidatorSetup<TModel, TProperty> IsRequired<TModel, TProperty>(this IModelPropertyValidatorSetup<TModel, TProperty> setup)
             where TProperty : class
         {
+            if (setup == null)
+            {
+                throw new ArgumentNullException(nameof(setup));
+            }
+
             return setup.IsInvalidWith(null);
         }
 
         public static IModelPropertyValidatorSetup<TModel, string> HasMaxLenght<TModel>(this IModelPropertyValidatorSetup<TModel, string> setup, int maxLenght)
         {
+            if (setup == null)
+            {
+                throw new ArgumentNullException(nameof(setup));
+            }
+
             var testStr = new string('0', maxLenght + 1);
             return setup.IsInvalidWith(testStr);
         }
 
         public static IModelPropertyValidatorSetup<TModel, string> HasMinLenght<TModel>(this IModelPropertyValidatorSetup<TModel, string> setup, int minLenght)
         {
+            if (setup == null)
+            {
+                throw new ArgumentNullException(nameof(setup));
+            }
+
             var testStr = new string('0', minLenght - 1);
             return setup.IsInvalidWith(testStr);
         }
 
         public static IModelPropertyValidatorSetup<TModel, int> InRange<TModel>(this IModelPropertyValidatorSetup<TModel, int> setup, int min, int max)
         {
+            if (setup == null)
+            {
+                throw new ArgumentNullException(nameof(setup));
+            }
+
             return setup
                 .IsInvalidWith(min - 1)
                 .IsInvalidWith(max + 1);
@@ -33,6 +53,11 @@ namespace ModelValidation.Test.Extensions
 
         public static IModelPropertyValidatorSetup<TModel, double> InRange<TModel>(this IModelPropertyValidatorSetup<TModel, double> setup, double min, double max)
         {
+            if (setup == null)
+            {
+                throw new ArgumentNullException(nameof(setup));
+            }
+
             return setup
                 .IsInvalidWith(min - 1)
                 .IsInvalidWith(max + 1);
--- src/ModelValidation.Test/ModelValidator.cs
+++ src/ModelValidation.Test/ModelValidator.cs
@@ -6,6 +6,11 @@ namespace ModelValidation.Test
     {
         public static void Test<TModel>(Func<TModel> createValidModelFunc, Action<IModelTestSetup<TModel>> setupAction) where TModel : class
         {
+            if (setupAction == null)
+            {
+                throw new ArgumentNullException(nameof(setupAction));
+            }
+
             var setup = new ModelTestSetup<TModel>(createValidModelFunc);
             setupAction(setup);
             setup.Run();
